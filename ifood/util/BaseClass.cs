974dfd9656dfba668c22335fd75275c74b066ca3
Priscila Dayana Campos Marques <priscila.dcmarques@gmail.com>
2019-02-02 20:35:04 -0200

    create BaseClass

---
+++ ifood/util/BaseClass.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System.Configuration;
+
+namespace projectcit.Utils
+{
+    public class BaseClass
+    {
+        public static IWebDriver driver = null;
+        string browser = "";
+        public static string url = "";
+
+        public BaseClass()
+        {
+            url = ConfigurationManager.AppSettings["URL"];
+            browser = ConfigurationManager.AppSettings["Browser"];
+        }
+
+        public void InstanceDriver()
+        {
+            switch (browser)
+            {
+                case "ie":
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+        }
+
+    }
+}
